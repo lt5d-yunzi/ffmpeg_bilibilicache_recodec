b5619062314090608ce6614489c1044121b05c0a
lt5d-yunzi <1315807391@qq.com>
2020-07-02 19:09:40 +0800

    Update start_linux.sh

--- start_linux.sh
+++ start_linux.sh
@@ -3,7 +3,7 @@ read -p "填写输入视频文件名称:" v
 read -p "填写输入音频文件名称:" a
 read -p "填写输出文件名称:" i
 echo 音频转码
-ffmpeg -i $a.m4s -vcodec copy -map_metadata -1 ./temp/audio.aac
+ffmpeg -i $a.m4s -acodec aac -map_metadata -1 ./temp/audio.aac
 echo 视频转码
 ffmpeg -i $v.m4s -vcodec copy -map_metadata -1 ./temp/video.mp4
 echo 音视混流
